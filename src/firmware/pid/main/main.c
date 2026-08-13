0e202138fe62594a45d39bc3ae4afd38ec1595de
Luis Gomez <gomezluis2907@gmail.com>
2026-08-13 14:30:06 -0600

    fix: correct rotor logic

--- src/firmware/pid/main/main.c
+++ src/firmware/pid/main/main.c
@@ -269,7 +269,7 @@ void pid_task(void *pvParameters)
         float rotor_0 = base_rpm - out_pitch - out_roll - out_yaw;
         float rotor_1 = base_rpm + out_pitch + out_roll - out_yaw;
         float rotor_2 = base_rpm - out_pitch + out_roll + out_yaw;
-        float rotor_3 = base_rpm + out_pitch - out_roll - out_yaw;
+        float rotor_3 = base_rpm + out_pitch - out_roll + out_yaw;
 
         // NaN fail-safe
         if (isnan(rotor_0) || isinf(rotor_0)) rotor_0 = 0.0f;
